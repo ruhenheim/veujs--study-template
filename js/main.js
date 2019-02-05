7e50ce4145cac85884cf56326b894e490321dced
ruhenheim <asd.uchiyama@gmail.com>
2019-02-06 01:35:45 +0900

    qiita記事検索app

--- js/main.js
+++ js/main.js
@@ -1,6 +1,42 @@
 let app = new Vue({
   el: '#app',
   data: {
-    message: 'Hello Vue.js!',
+    keyword: '',
+    items: null,
+    message: '',
   },
+  watch: {
+    keyword: function(newKeyword, oldKeyword) {
+      this.message = 'Waiting for you to stop typing...';
+      this.debouncedGetAnswer()
+    }
+  },
+  created: function() {
+    // this.keyword = 'JavaScript';
+    // this.getAnswer();
+    this.debouncedGetAnswer = _.debounce(this.getAnswer, 1000);
+  },
+  methods: {
+    getAnswer: function() {
+      if (this.keyword === '') {
+        this.items = null;
+        this.message = '';
+        return
+      }
+      this.message = 'Loding...';
+      const vm = this;
+      let params = { page: 1, per_page: 20, query: this.keyword }
+      axios.get('https://qiita.com/api/v2/items', { params })
+        .then(function(response) {
+          console.log(response);
+          vm.items = response.data;
+        })
+        .catch(function(error) {
+          vm.message = 'Error!:' + error;
+        })
+        .finally(function() {
+          vm.message = '';
+        })
+    }
+  }
 });
