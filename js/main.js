5fe6263376937a865df51706d2254318fc2a0c6f
ruhenheim <asd.uchiyama@gmail.com>
2019-02-11 10:13:27 +0900

    vuejs.org tutorial todos app

--- js/main.js
+++ js/main.js
@@ -1,6 +1,73 @@
-let app = new Vue({
+var STORAGE_KEY = 'todos-vuejs-demo'
+var todoStorage = {
+  fetch: function () {
+    var todos = JSON.parse(localStorage.getItem(STORAGE_KEY) || '[]')
+    todos.forEach(function(todo, index){
+      todo.id = index
+    })
+    todoStorage.uid = todos.length
+    return todos
+  },
+  save: function (todos) {
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(todos))
+  }
+}
+// -----------------------------
+
+const app = new Vue({
   el: '#app',
   data: {
     message: 'Hello Vue.js!',
+    todos: [],
+    options: [
+      { value: -1, label: 'すべて' },
+      { value: 0, label: '作業中' },
+      { value: 1, label: '完了' },
+    ],
+    current: -1
   },
-});
+  computed: {
+    computedTodos: function() {
+      return this.todos.filter(function(el) {
+        return this.current < 0 ? true : this.current === el.state
+      }, this)
+    },
+    labels() {
+      return this.options.reduce(function(root, option) {
+        return Object.assign(root, { [option.value]: option.label })
+      }, {})
+    }
+  },
+  watch: {
+    todos: {
+      handler: function(todos) {
+        todoStorage.save(todos)
+      },
+      deep: true //ネストしているデータも一緒に監視
+    }
+  },
+  methods: {
+    doAdd: function(event, value) {
+      var comment = this.$refs.comment
+      if (!comment.value.length) {
+        return
+      }
+      this.todos.push({
+        id: todoStorage.uid++,
+        comment: comment.value,
+        state: 0
+      })
+      comment.value = ''
+    },
+    doChangeState: function (item) {
+      item.state = item.state ? 0 : 1;
+    },
+    doRemove: function (item) {
+      var index = this.todos.indexOf(item)
+      this.todos.splice(index, 1)
+    }
+  },
+  created() {
+    this.todos = todoStorage.fetch()
+  }
+})
